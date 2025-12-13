2febe887b041fbb2a914d2b388958f7ff9559300
Andrew Dunn <adunn79@yahoo.com>
2025-12-13 15:37:14 -0800

    fix: Add missing permission_handler import in settings_screen

--- lib/features/settings/screens/settings_screen.dart
+++ lib/features/settings/screens/settings_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:go_router/go_router.dart';
 import 'package:audioplayers/audioplayers.dart';
 import 'package:sable/core/services/settings_control_service.dart';
